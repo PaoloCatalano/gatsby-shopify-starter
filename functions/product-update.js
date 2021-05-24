50eea57c2a75f5f6e02bbd96551f68b0e14fcfd1
Paolo Catalano <paolo12587@gmail.com>
2021-05-25 01:23:25 +0200

    product update function

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,69 @@
-exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello World',
-  });
+const faunadb = require('faunadb');
+const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+});
+
+exports.handler() = function (event, context, callback) {
+  const isValid = verifyWebhookIntegrity(
+    process.env.SHOPIFY_WEBHOOK_KEY,
+    event.headers['x-shopify-hmac-sha256'],
+    event.body
+  );
+
+  if (isValid) {
+    const body = JSON.parse(event.body);
+    const { id } = body;
+    delete body.updated_at;
+    body.variants.forEach(v => {
+      delete v.updated_at;
+      delete v.inventory_quantity;
+      delete v.old_inventory_quantity;
+    });
+
+    const bodyString = JSON.stringify(body);
+
+    client
+      .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+      .then(result => {
+        if (result.data.product !== bodyString) {
+          client
+            .query(
+              q.Update(result.ref, {
+                data: { product: bodyString },
+              })
+            )
+            .then(() => {
+              // call rebuild
+              axios.post(process.env.NETLIFY_BUILD_URL);
+            })
+            .catch(e => {
+              console.log('error update product:', e);
+            });
+        }
+      })
+      .catch(() => {
+        client
+          .query(
+            q.Create(q.Collection('products'), {
+              data: { id, product: bodyString },
+            })
+          )
+          .then(() => {
+            // call rebuild
+            axios.post(process.env.NETLIFY_BUILD_URL);
+          })
+          .catch(e => {
+            console.log('error in adding to db: ', e);
+          });
+      });
+  } else {
+    callback(null, {
+      statusCode: 403,
+      body: '403 Error',
+    });
+  }
 };
